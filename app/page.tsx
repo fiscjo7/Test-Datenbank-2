6fc5d43ff467ecb8007fc3b6d53ff5e0e1a30981
Jorge Fischer <19788@nordakademie.de>
2026-05-28 11:49:40 +0200

    Remove Next.js stack and keep static index.html app only

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ManualSelectorCard } from "@/components/ManualSelectorCard";
-
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center p-4 sm:p-6">
-      <ManualSelectorCard />
-    </main>
-  );
-}
